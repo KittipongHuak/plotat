6b110801abd19a3d5d2c08cd2f9066c2e1522cbd
KittipongHuak <97436073+KittipongHuak@users.noreply.github.com>
2022-04-06 16:18:52 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+namespace screenMagic {
+    export function plotAt(index: number): void {
+        index |= 0
+        const y = Math.floor(index / 5);
+        const x = Math.floor(index % 5);
+    }
+}
